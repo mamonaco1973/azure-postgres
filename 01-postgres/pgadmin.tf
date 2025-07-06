1e36f76480856aa048325368f7f12a6309aa48d1
Mike Monaco <mamonaco1973@gmail.com>
2025-07-06 13:02:51 -0400

    add pgadmin.tf

---
+++ 01-postgres/pgadmin.tf
@@ -0,0 +1,51 @@
+resource "azurerm_log_analytics_workspace" "log_analytics" {
+  name                = "${var.project_resource_group}-logs"
+  location            = azurerm_resource_group.project_rg.location
+  resource_group_name = azurerm_resource_group.project_rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_container_app_environment" "env" {
+  name                       = "${var.project_resource_group}-env"
+  location                   = azurerm_resource_group.project_rg.location
+  resource_group_name        = azurerm_resource_group.project_rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.log_analytics.id
+}
+
+resource "azurerm_container_app" "pgadmin" {
+  name                         = "pgadmin"
+  container_app_environment_id = azurerm_container_app_environment.env.id
+  resource_group_name          = azurerm_resource_group.project_rg.name
+  revision_mode                = "Single"
+
+  template {
+    container {
+      name   = "pgadmin"
+      image  = "dpage/pgadmin4:latest"
+      cpu    = 0.5
+      memory = "1Gi"
+
+      env {
+        name  = "PGADMIN_DEFAULT_EMAIL"
+        value = "admin@local"
+      }
+
+      env {
+        name  = "PGADMIN_DEFAULT_PASSWORD"
+        value = "Password1!"
+      }
+    }
+
+    scale {
+      min_replicas = 1
+      max_replicas = 1
+    }
+  }
+
+  ingress {
+    external_enabled = true
+    target_port      = 80
+    transport        = "auto"
+  }
+}
