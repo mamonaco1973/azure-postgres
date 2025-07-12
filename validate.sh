b1c2d5e3dbb7c4087848e794216fcce87ae82445
mamonaco1973 <mamonaco1973@gmail.com>
2025-07-12 22:14:33 +0000

    add validate.sh

---
+++ validate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#-------------------------------------------------------------------------------
+# Output pgweb URL and postgres DNS name
+#-------------------------------------------------------------------------------
+
+PGWEB_DNS_NAME=$(az network public-ip show \
+  --name pgweb-vm-public-ip \
+  --resource-group postgres-rg \
+  --query "dnsSettings.fqdn" \
+  --output tsv)
+
+echo "NOTE: pgweb running at http://$PGWEB_DNS_NAME"
+
+PG_DNS=$(az postgres flexible-server list \
+  --resource-group postgres-rg \
+  --query "[?starts_with(name, 'postgres-instance')].fullyQualifiedDomainName" \
+  --output tsv)
+
+echo "NOTE: Hostname for postgres server is $PG_DNS"
+
+#-------------------------------------------------------------------------------
+# END OF SCRIPT
+#-------------------------------------------------------------------------------
